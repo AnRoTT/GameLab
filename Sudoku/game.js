b1c2bc24326fa375f63cddcd53dd02c4f3848f06
AnRoTT <andi.rollinger@hotmail.de>
2026-09-13 21:24:25 +0200

    Fix Sudoku-OCR JavaScript-Syntaxfehler

--- Sudoku/game.js
+++ Sudoku/game.js
@@ -1472,7 +1472,7 @@
             && (!second || best.bestConfidence - second.bestConfidence >= 28);
     }
 
-    async async function recognizeSudokuCells(worker, source, statusElement) {
+    async function recognizeSudokuCells(worker, source, statusElement) {
         const result = new Map();
         const uncertain = [];
         let processed = 0;
